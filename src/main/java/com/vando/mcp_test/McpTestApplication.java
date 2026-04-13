bbf09610efcb351b209dd907057bcb6a1ec18979
mvd <mvd@acadon.de>
2026-04-13 10:11:40 +0200

    first funktionable state

---
+++ src/main/java/com/vando/mcp_test/McpTestApplication.java
@@ -0,0 +1,13 @@
+package com.vando.mcp_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vando/mcp_test/McpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vando.mcp_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
